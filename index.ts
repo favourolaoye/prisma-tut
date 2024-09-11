293a5a892a1c1b02409a107d7ddd7cd77b280b32
0x46641766F7572 <96478305+favourolaoye@users.noreply.github.com>
2024-09-11 18:51:21 +0100

    create main func

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main(){
+
+}
+
+main()
+.then( async () => {
+    prisma.$disconnect()
+})
+.catch((e)=> {
+    console.error(e);
+    prisma.$disconnect();
+    process.exit(1);
+})
